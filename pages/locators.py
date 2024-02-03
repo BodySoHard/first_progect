8f45641c243e539ec3251960548c90ecbe923319
DavydovDY <denis.davydov@vitech.team>
2024-02-03 15:40:55 +0300

    test_guest_can_add_product_to_basket

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,9 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    NAME_PRODUCT_BOOK = (By.CSS_SELECTOR, ".product_main h1")
+    NAME_PRODUCT_BOOK_IN_BASKET = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > div > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
---
+++ pages/product_page.py
@@ -0,0 +1,13 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+
+    def add_product_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to_basket.click()
+
+    def check_name_product(self):
+        name_product = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_BOOK)
+        name_product_in_basket = self.browser.find_element(*ProductPageLocators.NAME_PRODUCT_BOOK_IN_BASKET)
+        assert name_product.text == name_product_in_basket.text, "Product name in basket and page not the same"
